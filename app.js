ed32284dedb0607dc158b712a3a859392a0ed13e
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-04-16 14:33:26 -0600

    Attempt to add drawing functionality
    mousedown eventListener has been added to each div on grid creation,
    calling the changeColor function on trigger. However, the listener doesn't
    seem to trigger at all. Curently working on finding a fix.

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-function generateGrid() {
-    const container = document.querySelector('.container');
-    for(let i = 0; i < (16*16); i++){
+const DEFAULT_COLOR = 'red';
+const DEFAULT_SIZE = 16;
+const DEFAULT_MODE = 'singleColor';
+
+const container = document.querySelector('.container');
+
+let color = DEFAULT_COLOR;
+let size = DEFAULT_SIZE;
+let mode = DEFAULT_MODE;
+
+function generateGrid(size) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for(let i = 0; i < (size * size); i++){
         const div = document.createElement('div');
+        div.addEventListener('mousedown', changeColor);
+
         container.appendChild(div);
     }
 }
 
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+
+    e.target.style.backgroundColor = color;
+    console.log('done');
+}
 
 generateGrid()
