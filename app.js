ecbf35da2847a52523d3a311b19c1dded67d7d04
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-04-16 14:00:59 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function generateGrid() {
+    const container = document.querySelector('.container');
+    for(let i = 0; i < (16*16); i++){
+        const div = document.createElement('div');
+        container.appendChild(div);
+    }
+}
+
+
+generateGrid()
